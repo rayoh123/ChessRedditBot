25ed9629c9851f502a429e88c00a525d20e22b9b
Raymond Ouyang <ouyangmingraymond@gmail.com>
2019-09-12 17:45:13 -0700

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@
 ###   
 ###
 
-
+import time
 import praw
 import details
 import re
@@ -32,32 +32,40 @@ express2 = '([Ww]hite to )'
 express3 = '([Bb]lack to )'
 turn = ''
 winner = ''
-for submission in reddit_instance.subreddit('chess').new(limit=250):
-    
-    if submission.id not in commented       and \
-    re.search(express, submission.title)    and \
-    submission.selftext==""                 and \
-    re.match(url_express, submission.url):
-        
-        if re.search(express2, submission.title):
-            turn = 'white'
-        else:
-            turn = 'black'
-            
-        try:
-            fen = helpers.grab_fen(submission.url, turn)
-            score, advantage, line = helpers.grab_line(fen, turn)
-        except:
-            print("Just skipped this one: ", submission.url)
-            continue
 
-        if type(advantage) == float:
-            submission.reply(f"I'm a bot and I solved it! {score} has an advantage of >!{advantage} pawns!<. The best continuation is: {line}")
-        else:
-            submission.reply(f"I'm a bot and I solved it! {score} has an advantage of >!{advantage}!<. The best continuation is: {line}")
-            
-        print("Just solved this one: ", submission.url)
+while True:
+    for submission in reddit_instance.subreddit('chess').new(limit=300):
         
-        commented.append(submission.id)
-        with open('commented.txt', 'a') as f:
-            f.write(submission.id + '\n')
+        if submission.id not in commented       and \
+        re.search(express, submission.title)    and \
+        submission.selftext==""                 and \
+        re.match(url_express, submission.url):
+            
+            if re.search(express2, submission.title):
+                turn = 'white'
+            else:
+                turn = 'black'
+                
+            try:
+                fen = helpers.grab_fen(submission.url, turn)
+                score, advantage, line = helpers.grab_line(fen, turn)
+            except:
+                print("Just skipped this one: ", submission.url)
+                with open('commented.txt', 'a') as f:
+                    f.write(submission.id + '\n')
+                continue
+
+            if type(advantage) == float:
+                submission.reply(f"I'm a bot and I solved it! >!{score}!< has an advantage of >!{advantage} pawns!<. The best continuation is: {line}")
+            else:
+                submission.reply(f"I'm a bot and I solved it! >!{score}!< has an advantage of >!{advantage}!<. The best continuation is: {line}")
+                
+            print("Just solved this one: ", submission.url)
+            
+            commented.append(submission.id)
+            with open('commented.txt', 'a') as f:
+                f.write(submission.id + '\n')
+
+                
+    time.sleep(1200)
+        
