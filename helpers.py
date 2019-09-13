3a5465d7c301ca740788a44fd279df441622f64a
Raymond Ouyang <ouyangmingraymond@gmail.com>
2019-09-12 22:08:19 -0700

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -10,7 +10,7 @@ def grab_fen(url: str, turn: str) -> str:
     position in the image the url leads to.
     '''
     
-    complete_string = "cd C:\\Users\\Raymond\\Desktop\\Chess Bot\\tensorflow_chessbot-chessfenbot && tensorflow_chessbot.py --url %s" % (url)
+    complete_string = "cd C:\\YOUR FILE PATH HERE\\Chess Bot\\tensorflow_chessbot-chessfenbot && tensorflow_chessbot.py --url %s" % (url)
     r = str(subprocess.check_output(complete_string, shell=True, timeout=5))
     if float(re.search('(\d{2,3})(.\d%)', r).group(0)[:-1]) < 60.0:
         print("The image linked below isn't clear to me", float(re.search('(\d{2,3})(.\d%)', r).group(0)[:-1]))
