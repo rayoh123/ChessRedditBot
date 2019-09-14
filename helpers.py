25f95be12eef7fe36e2d148e4bca801e85382636
Raymond Ouyang <ouyangmingraymond@gmail.com>
2019-09-13 17:41:52 -0700

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,15 +2,58 @@ import chess.engine
 import subprocess
 import re
 
-def grab_fen(url: str, turn: str) -> str:
+def reverse_fen(fen: str) -> str:
     '''
-    This function takes a string representing a url
-    and another string identifying whether it is white's or black's turn.
-    It then outputs a string representing the FEN of the chess
-    position in the image the url leads to.
+    This function takes a FEN and outputs the reverse
+    of the FEN.
     '''
     
-    complete_string = "cd C:\\YOUR FILE PATH HERE\\Chess Bot\\tensorflow_chessbot-chessfenbot && tensorflow_chessbot.py --url %s" % (url)
+    if fen[-3:] in (' w ', ' b '):
+        return fen[:-3][::-1] + fen[-3:]
+    return fen[::-1]
+
+def is_flipped(fen: str, turn: str) -> bool:
+    '''
+    This function takes a FEN and a string identifying
+    whether it is white's or black's turn and outputs a
+    boolean representing whether the FEN displays the player
+    whose turn it is at the bottom of the board.
+    '''
+
+    middle_idx = [i for i, n in enumerate(fen) if n == '/'][4]
+    first_upper = 0
+    second_upper = 0
+
+    for i in range(middle_idx):
+        if fen[i].isupper():
+            first_upper += 1
+    for i in range(middle_idx, len(fen)):
+        if fen[i].isupper():
+            second_upper += 1
+
+    if first_upper<second_upper:
+        if turn == 'white':
+            return False
+        else:
+            return True
+    elif first_upper>second_upper:
+        if turn == 'white':
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def grab_fen(url: str, turn: str) -> str:
+    '''
+    This function takes a url and a string identifying
+    whether is white's or black's turn. It then outputs
+    a string representing the FEN of the chess position
+    in the image the url leads to.
+    '''
+    
+    complete_string = "cd C:\\Users\\Raymond\\Desktop\\Chess Bot\\tensorflow_chessbot-chessfenbot && tensorflow_chessbot.py --url %s" % (url)
     r = str(subprocess.check_output(complete_string, shell=True, timeout=5))
     if float(re.search('(\d{2,3})(.\d%)', r).group(0)[:-1]) < 70.0:
         print("The image linked below isn't clear to me", float(re.search('(\d{2,3})(.\d%)', r).group(0)[:-1]))
@@ -19,19 +62,27 @@ def grab_fen(url: str, turn: str) -> str:
     end_idx = r.index('\\r\\nFinal Certainty')
 
     if turn == 'white':
-        return r[(begin_idx+15):end_idx] + ' w '
-    else:
-        return r[(begin_idx+15):end_idx][::-1] + ' b '
         
+        if is_flipped(r[(begin_idx+15):end_idx], 'white'):
+            return reverse_fen(r[(begin_idx+15):end_idx]) + ' w '
+        return r[(begin_idx+15):end_idx] + ' w '
     
+    else:
+        
+        if is_flipped(r[(begin_idx+15):end_idx], 'black'):
+            return r[(begin_idx+15):end_idx] + ' b '
+        return reverse_fen(r[(begin_idx+15):end_idx]) + ' b '
+        
+
 
 
 def grab_line(fen: str, turn: str) -> str or float:
     '''
-    This function takes a FEN and a string identifying whether it is white's or
-    black's turn. It then outputs whether white or black has the advantage
-    in the chess position represented by the FEN, the advantage, and the best
-    continuation as judged by Stockfish 10.
+    This function takes a FEN and a string identifying
+    whether it is white's or black's turn. It then outputs
+    whether white or black has the advantage in the chess
+    position represented by the FEN, the advantage, and the
+    best continuation as judged by Stockfish 10.
     '''
 
     engine = chess.engine.SimpleEngine.popen_uci("stockfish_10_x64.exe")
@@ -65,5 +116,4 @@ def grab_line(fen: str, turn: str) -> str or float:
         final_line += f"\n\n>!{move}!<"
         
     return score, advantage, final_line
-
 
